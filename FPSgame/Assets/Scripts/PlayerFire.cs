91e309a00c3ed87948a7e2e85957c70cccb82b47
94mark <90877724+94mark@users.noreply.github.com>
2022-02-02 21:21:29 +0900

    피격 이펙트 세팅

--- FPSgame/Assets/Scripts/PlayerFire.cs
+++ FPSgame/Assets/Scripts/PlayerFire.cs
@@ -7,6 +7,14 @@ public class PlayerFire : MonoBehaviour
     public GameObject firePosition; //발사 위치
     public GameObject bombFactory; //투척 무기 오브젝트
     public float throwPower = 15f; //투척 파워
+    public GameObject bulletEffect; //피격 이펙트 오브젝트
+    ParticleSystem ps; //피격 에픽트 파티클 시스템
+
+    void Start()
+    {
+        //피격 이펙트 오브젝트에서 파티클 시스템 컴포넌트 
+        ps = bulletEffect.GetComponent<ParticleSystem>();
+    }
 
     void Update()
     {
@@ -24,6 +32,28 @@ public class PlayerFire : MonoBehaviour
 
             //카메라의 정면 방향으로 수류탄에 물리적 힘을 가함
             rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
+
+        //마우스 왼쪽 버튼을 누르면 시선이 바라보는 방향으로 총 발사
+
+        //마우스 왼쪽 버튼 입력
+        if(Input.GetMouseButtonDown(0))
+        {
+            //레이를 생성한 후 발사될 위치와 진행 방향 설정
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            //레이가 부딪힌 대상의 정보를 저장할 변수를 생성
+            RaycastHit hitInfo = new RaycastHit();
+
+            //레이를 발사한 후 만일 부딪힌 물체가 있으면 피격 이벤트 실행
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                //피격 이펙트의 위치를 레이가 부딪힌 지점으로 이동
+                bulletEffect.transform.position = hitInfo.point;
+
+                //피격 이펙트 플레이
+                ps.Play();
+            }
         }
     }
 }
