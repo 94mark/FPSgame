f761ddba1697c81d3419fe19d180b2e14bfcb733
94mark <90877724+94mark@users.noreply.github.com>
2022-02-02 19:41:41 +0900

    Update PlayerMove.cs
    이동 시 중력 적용

--- FPSgame/Assets/Scripts/PlayerMove.cs
+++ FPSgame/Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 7f; //이동 속도 변수
+    CharacterController cc; //캐릭터 콘트롤러 변수
+    float gravity = -20f; //중력 변수
+    float yVelocity = 0; //수직 속력 변수
+
+    void Start()
+    {
+        //캐릭터 컨트롤러 컴포넌트
+        cc = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -19,7 +28,11 @@ public class PlayerMove : MonoBehaviour
         //2-1. 메인 카메라를 기준으로 방향 변환
         dir = Camera.main.transform.TransformDirection(dir);
 
+        //2-2. 캐릭터 수직 속도에 중력 값을 적용
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
         //3. 속도에 맞춰 이동
-        transform.position += dir * moveSpeed * Time.deltaTime;
+        cc.Move(dir * moveSpeed * Time.deltaTime);
     }
 }
