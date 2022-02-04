e37b63e6eb4267b84ff129e17e39292bcfbbe4c8
94mark <90877724+94mark@users.noreply.github.com>
2022-02-04 16:07:47 +0900

    Update GameManager.cs
    게임 옵션(종료/재시작/계속) 기능 구현

--- FPSgame/Assets/Scripts/GameManager.cs
+++ FPSgame/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class GameManager : MonoBehaviour
     {
         Ready,
         Run,
+        Pause,
         GameOver
     }
 
@@ -32,6 +34,7 @@ public class GameManager : MonoBehaviour
     public GameObject gameLabel;
     //PlayerMove 클래스 변수
     PlayerMove player;
+    public GameObject gameOption; //옵션 화면 UI 오브젝트 변수
 
     void Start()
     {
@@ -81,8 +84,49 @@ public class GameManager : MonoBehaviour
             gameText.text = "Game Over";
             //상태 텍스트의 색상을 붉은색으로 한다
             gameText.color = new Color32(255, 0, 0, 255);
+            //상태 텍스트의 자식 오브젝트의 트랜스폼 컴포넌트
+            Transform buttons = gameText.transform.GetChild(0);
+            //버튼 오브젝트 활성화
+            buttons.gameObject.SetActive(true);
             //상태를 '게임 오버' 상태로 변경
             gState = GameState.GameOver;
         }
+    }
+
+    //옵션 화면 켜기
+    public void OpenOptionWindow()
+    {
+        //옵션 창을 활성화
+        gameOption.SetActive(true);
+        //게임 속도를 0배속으로 전환
+        Time.timeScale = 0f;
+        //게임 상태를 일시 정지 상태로 변경
+        gState = GameState.Pause;
+    }
+
+    //계속하기 옵션
+    public void CloseOptionWindow()
+    {
+        //옵션 창을 비활성화
+        gameOption.SetActive(false);
+        //게임 속도를 1배속으로 전환
+        Time.timeScale = 1f;
+        //게임 상태를 게임 중 상태로 변경
+        gState = GameState.Run;
+    }
+
+    //다시하기 옵션
+    public void RestartGame()
+    {
+        //게임 속도를 1배속으로 전환
+        Time.timeScale = 1f;
+        //현재 씬 번호를 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    //게임 종료 옵션
+    public void QuitGame()
+    {
+        //애플리케이션 종료
+        Application.Quit();
     }
 }
