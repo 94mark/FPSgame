cada456051685b97c5e1ea51e4d85bda28531be0
94mark <90877724+94mark@users.noreply.github.com>
2022-02-04 15:00:34 +0900

    Update BombAction.cs
    OverlapSphere()함수를 사용해 폭발 범위 내 모든 에너미 검출, 반복문으로 폭발 범위 내에 모든 에너미에게 데미지 적용

--- FPSgame/Assets/Scripts/BombAction.cs
+++ FPSgame/Assets/Scripts/BombAction.cs
@@ -6,10 +6,22 @@ public class BombAction : MonoBehaviour
 {
     //폭발 이펙트 프리팹 변수
     public GameObject bombEffect;
+    //수류탄 데미지
+    public int attackPower = 10;
+    //폭발 효과 반경
+    public float explosionRadius = 5f;
 
     //충돌했을 때의 처리
     private void OnCollisionEnter(Collision collision)
     {
+        //폭발 효과 반경 내에서 레이어가 Enemy인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장
+        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 10);
+
+        //저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지 적용
+        for(int i = 0; i < cols.Length; i++)
+        {
+            cols[i].GetComponent<EnemyFSM>().HitEnemy(attackPower);
+        }
         //이펙트 프리팹 생성
         GameObject eff = Instantiate(bombEffect);
 
